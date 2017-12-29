b4c02494f7e0d717198bc9f667fe1a8e0c4d5095
Nitin Srivastava <nitin.srivastava@alepo.com>
2017-12-29 16:14:08 +0000

    Recommited src files

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,7 +2,7 @@ public class Rectangle {
   public int length;
   public int width;
 
-  public Rectangle (int length, int width) {
+  public Rectangle(int length, int width) {
     this.length = length;
     this.width = width;
   }
@@ -12,6 +12,6 @@ public class Rectangle {
   }
 
   public int getPerimeter() {
-    return 2*(length + width);
+    return 2 * (length + width);
   }
 }
